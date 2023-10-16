c6a103f31acbe01a3a061e4c846fbd9108682885
zed.zhao <954489346zyz@sina.com>
2023-10-16 09:42:59 +0800

    feat: 定时任务update

--- src/wechaty/schedule.js
+++ src/wechaty/schedule.js
@@ -98,6 +98,6 @@ export async function initSchedule(bot) {
 
     const room = await getRoom(bot);
     // room.say('⭐️ AI 助手升级完毕，新增以下功能：优化十月的语言生成，使之更加具有亲和力"。[Ver: 1.9]');
-    room.say('⭐️ 重启成功。[Ver: 1.9]');
+    // room.say('⭐️ 重启成功。[Ver: 1.9]');
   });
 }
