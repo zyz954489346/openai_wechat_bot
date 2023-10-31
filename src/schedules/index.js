ec97ce9b2339459258809107d2087aacd23eaba9
zed.zhao <954489346zyz@sina.com>
2023-10-31 18:35:16 +0800

    feat: 代码优化

--- src/schedules/index.js
+++ src/schedules/index.js
@@ -11,21 +11,21 @@ export const initSchedule = async (bot) => {
   })
 
   // 早安 8:30
-  schedule.scheduleJob('0 30 8 * * *', () => goodMorning(bot));
+  schedule.scheduleJob('0 30 8 * * *', async() => {await goodMorning(bot)});
 
   // 上班打卡 9:00
-  schedule.scheduleJob('0 0 9 * * *', () => punchForWork(bot));
+  schedule.scheduleJob('0 0 9 * * *', async () => {await punchForWork(bot)});
 
   // 午安 12:00
-  schedule.scheduleJob('0 0 12 * * *', () => goodNoon(bot));
+  schedule.scheduleJob('0 0 12 * * *', async () => {await goodNoon(bot)});
 
   // 下班打卡 18:30
-  schedule.cancelJob('0 30 18 * * *', () => punchForHome(bot));
+  schedule.cancelJob('0 30 18 * * *', async () => {await punchForHome(bot)});
 
   // 更新日志
   // schedule.scheduleJob(new Date(Date.now() + 5000), () => versionLog(bot));
 
   // 启动消息
-  schedule.scheduleJob(new Date(Date.now() + 5000), () => startUp(bot));
+  schedule.scheduleJob(new Date(Date.now() + 5000), async () => {await startUp(bot)});
 };
 
