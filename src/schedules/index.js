c18c5318e9c6e8bf7d3434f72f4d90506f83ba67
zed.zhao <954489346zyz@sina.com>
2023-10-31 17:28:12 +0800

    feat: 代码优化

--- src/schedules/index.js
+++ src/schedules/index.js
@@ -23,7 +23,7 @@ export const initSchedule = async (bot) => {
   schedule.cancelJob('0 30 18 * * *', () => punchForHome(bot));
 
   // 更新日志
-  schedule.scheduleJob(new Date(Date.now() + 5000), versionLog(bot));
+  // schedule.scheduleJob(new Date(Date.now() + 5000), () => versionLog(bot));
 
   // 启动消息
   schedule.scheduleJob(new Date(Date.now() + 5000), () => startUp(bot));
