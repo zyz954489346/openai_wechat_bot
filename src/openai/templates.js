c19e04d02ed6879922e8cf1c97ce47c22fc60d91
zed.zhao <954489346zyz@sina.com>
2023-11-22 09:42:54 +0800

    feat: 代码优化

--- src/openai/templates.js
+++ src/openai/templates.js
@@ -1,6 +1,4 @@
-export const goodMorningTpl = `
-请用生动的语言和群友们说早安，同时保证早安的内容要符合中国人的语言习惯和理解方式。
-`;
+export const goodMorningTpl = `请用生动的语言和群友们说早安，并简单介绍下今天的天气。`;
 
 export const punchForWorkTpl = `请和我说早安，并提醒我上班打卡。`;
 
