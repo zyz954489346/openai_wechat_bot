4f47cc3b851fc6cd2f90cb457244efa38c73c344
zed.zhao <954489346zyz@sina.com>
2023-12-12 15:42:05 +0800

    feat: 代码优化

--- src/resources/holiday/index.js
+++ src/resources/holiday/index.js
@@ -1,6 +1,8 @@
 import holiday2023 from './2023.js'
+import holiday2024 from './2024.js'
 
 // @see https://timor.tech/api/holiday
 export default {
   '2023': holiday2023,
+  '2024': holiday2024,
 }
